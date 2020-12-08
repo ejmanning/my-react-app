f77eb95b831e8442f767e75475e7615b92771061
Erica Manning <60369190+ejmanning@users.noreply.github.com>
2020-12-08 16:54:54 -0600

    Create button.js

---
+++ my-react-app/src/button.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Button = (props) => {
+    const {buttonText, onClickFunction} = props;
+    // same as saying const buttonText = props.buttonText;
+
+    return (
+        <button onClick={onClickFunction}>{buttonText}</button>
+    );
+};
+
+export {Button};
